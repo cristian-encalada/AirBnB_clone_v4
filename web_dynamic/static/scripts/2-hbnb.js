fc6651d4d494b6336b96b0bff5491a66f4b85a8e
solp22 <sol.puentereal@gmail.com>
2023-09-25 13:45:36 -0700

    [3] change url to localhost

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
     function apiStatus() {
       $.ajax({
         type: 'GET',
-        url: 'http://0.0.0.0:5001/api/v1/status/',
+        url: 'http://localhost:5001/api/v1/status/',
         success: function (data) {
-          if (data.status == 'OK') {
+          if (data.status === 'OK') {
             $('#api_status').addClass('available');
           } else {
             $('#api_status').removeClass('available');
