7c03450906f14a5e0dbf6ca5db7d6a08051ab439
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2023-09-28 17:41:33 -0300

    Add javascript code to handle city filter

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,97 +1,133 @@
 $(document).ready(function () {
-    // console.log('Document ready function executed');
-    let checkedAmenities = [];
-    let checkedStates = [];
-  
-    function apiStatus () {
-      $.ajax({
-        type: 'GET',
-        url: 'http://localhost:5001/api/v1/status/',
-        success: function (data) {
-          if (data.status === 'OK') {
-            $('#api_status').addClass('available');
-          } else {
-            $('#api_status').removeClass('available');
-          }
+  // console.log('Document ready function executed');
+  let checkedAmenities = [];
+  let checkedStates = [];
+  let checkedCities = [];
+
+  function apiStatus () {
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:5001/api/v1/status/',
+      success: function (data) {
+        if (data.status === 'OK') {
+          $('#api_status').addClass('available');
+        } else {
+          $('#api_status').removeClass('available');
         }
-      });
-    }
-  
-    apiStatus();
-  
-    // Select all amenity input checkboxes
-    $('.amenities input[type="checkbox"]').change(function () {
-      const amenityId = $(this).data('id');
-      const amenityName = $(this).data('name');
-  
-      // Find if the amenity object is already in checkedAmenities
-      const existingAmenity = checkedAmenities.find(function (amenity) {
-        return amenity.id === amenityId;
-      });
-  
-      // Verify if the checkbox is checked or not
-      if ($(this).is(':checked')) {
-        if (!existingAmenity) {
-        // Push the amenity object if it doesn't exist in the array
-          checkedAmenities.push({ id: amenityId, name: amenityName });
-        }
-      } else {
-      // If unchecked, remove the amenity object that matches the Id
-        checkedAmenities = checkedAmenities.filter(function (amenity) {
-          return amenity.id !== amenityId;
-        });
       }
-  
-      // Update the <h4> tag with the list of amenity names
-      const amenitiesList = checkedAmenities.map(function (amenity) {
-        return amenity.name;
-      }).join(', ');
-  
-      $('.amenities h4').text('Amenities: ' + amenitiesList);
-  
-      // Check the state of checkedAmenities
-      console.log('Checked Amenities:', checkedAmenities);
+    });
+  }
+
+  apiStatus();
+
+  // Select all amenity input checkboxes
+  $('.amenities input[type="checkbox"]').change(function () {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    // Find if the amenity object is already in checkedAmenities
+    const existingAmenity = checkedAmenities.find(function (amenity) {
+      return amenity.id === amenityId;
     });
 
-    // Select all state input checkboxes
-    $('.locations input[type="checkbox"]').change(function () {
-        console.log('State checkbox changed'); 
-        const stateId = $(this).data('id');
-        const stateName = $(this).data('name');
-    
-        // Find if the state object is already in checkedStates
-        const existingState = checkedStates.find(function (state) {
-         return state.id === stateId;
-        });
-    
-        // Verify if the checkbox is checked or not
-        if ($(this).is(':checked')) {
-          if (!existingState) {
-          // Push the state object if it doesn't exist in the array
-            checkedStates.push({ id: stateId, name: stateName });
-          }
-        } else {
-        // If unchecked, remove the state object that matches the Id
-          checkedStates = checkedStates.filter(function (state) {
-            return state.id !== stateId;
-          });
-        }
-    
-        // Update the <h4> tag with the list of state names
-        const statesList = checkedStates.map(function (state) {
-          return state.name;
-        }).join(', ');
-    
-        $('.locations h4').text('States: ' + statesList);
-    
-        // Check the state of checkedStates
-        console.log('Checked States:', checkedStates);
+    // Verify if the checkbox is checked or not
+    if ($(this).is(':checked')) {
+      if (!existingAmenity) {
+        // Push the amenity object if it doesn't exist in the array
+        checkedAmenities.push({ id: amenityId, name: amenityName });
+      }
+    } else {
+      // If unchecked, remove the amenity object that matches the Id
+      checkedAmenities = checkedAmenities.filter(function (amenity) {
+        return amenity.id !== amenityId;
       });
-  
-    // Auxiliar function to create an article element tag for a place
-    // !== 1 ? 's' : '' check if place.x is not equal to 1. If not, it adds an "s" to the end of the word
-    function createArticle (place) {
-      const article = `
+    }
+
+    // Update the <h4> tag with the list of amenity names
+    const amenitiesList = checkedAmenities.map(function (amenity) {
+      return amenity.name;
+    }).join(', ');
+
+    $('.amenities h4').text('Amenities: ' + amenitiesList);
+
+    // Check the state of checkedAmenities
+    console.log('Checked Amenities:', checkedAmenities);
+  });
+
+  // Select all state input checkboxes
+  $('.locations input[type="checkbox"]').change(function () {
+    // console.log('State checkbox changed');
+    const stateId = $(this).data('id');
+    const stateName = $(this).data('name');
+
+    // Find if the state object is already in checkedStates
+    const existingState = checkedStates.find(function (state) {
+      return state.id === stateId;
+    });
+
+    // Verify if the checkbox is checked or not
+    if ($(this).is(':checked')) {
+      if (!existingState) {
+        // Push the state object if it doesn't exist in the array
+        checkedStates.push({ id: stateId, name: stateName });
+      }
+    } else {
+      // If unchecked, remove the state object that matches the Id
+      checkedStates = checkedStates.filter(function (state) {
+        return state.id !== stateId;
+      });
+    }
+
+    // Select all city input checkboxes
+    $('.locations ul li input[type="checkbox"]').change(function () {
+      // console.log('City checkbox changed');
+      const cityId = $(this).data('id');
+      const cityName = $(this).data('name');
+
+      // Find if the city object is already in checkedCities
+      const existingCity = checkedCities.find(function (city) {
+        return city.id === cityId;
+      });
+
+      // Verify if the checkbox is checked or not
+      if ($(this).is(':checked')) {
+        if (!existingCity) {
+          // Push the city object if it doesn't exist in the array
+          checkedCities.push({ id: cityId, name: cityName });
+        }
+      } else {
+        // If unchecked, remove the city object that matches the Id
+        checkedCities = checkedCities.filter(function (city) {
+          return city.id !== cityId;
+        });
+      }
+
+      // Update the <h4> tag with the list of city names
+      const citiesList = checkedCities.map(function (city) {
+        return city.name;
+      }).join(', ');
+
+      $('.locations h4').text('Cities: ' + citiesList);
+
+      // Check the state of checkedCities
+      console.log('Checked Cities:', checkedCities);
+    });
+
+    // Update the <h4> tag with the list of state names
+    const statesList = checkedStates.map(function (state) {
+      return state.name;
+    }).join(', ');
+
+    $('.locations h4').text('States: ' + statesList);
+
+    // Check the state of checkedStates
+    console.log('Checked States:', checkedStates);
+  });
+
+  // Auxiliar function to create an article element tag for a place
+  // !== 1 ? 's' : '' check if place.x is not equal to 1. If not, it adds an "s" to the end of the word
+  function createArticle (place) {
+    const article = `
             <article>
                 <div class="title_box">
                     <h2>${place.name}</h2>
@@ -107,53 +143,58 @@ $(document).ready(function () {
                 </div>
             </article>
         `;
-      return article;
-    }
-  
-    function postRequest () {
-      // Extract just the amenity IDs from checkedAmenities
-      const amenityIds = checkedAmenities.map(function (amenity) {
-        return amenity.id;
-      });
+    return article;
+  }
 
-       // Extract just the state IDs from checkedStates
-      const stateIds = checkedStates.map(function (state) {
-        return state.id;
-      });
-
-      // Create a data object to send in the request
-      const requestData = {
-        amenities: amenityIds,
-        states: stateIds,
-      };
-
-      // Check if both amenities and states arrays are empty
-      if (!requestData.amenities.length && !requestData.states.length) {
-        // If both are empty, send an empty object to retrieve all places
-        requestData.amenities = requestData.states = {};
-      }
-  
-      $.ajax({
-        type: 'POST',
-        url: 'http://0.0.0.0:5001/api/v1/places_search/',
-        contentType: 'application/json',
-        data: JSON.stringify(requestData),
-        success: function (data) {
-          $('.places').empty(); // Clear existing results
-          for (const place of data) {
-            const article = createArticle(place);
-            $('.places').append(article);
-          }
-          console.log('Post request done successfully');
-        }
-      });
-    }
-  
-    postRequest();
-  
-    // Event listener for the "Search" button
-    $('button[type="button"]').click(function () {
-      postRequest();
+  function postRequest () {
+    // Extract just the amenity IDs from checkedAmenities
+    const amenityIds = checkedAmenities.map(function (amenity) {
+      return amenity.id;
     });
+
+    // Extract just the state IDs from checkedStates
+    const stateIds = checkedStates.map(function (state) {
+      return state.id;
+    });
+
+    // Extract just the city IDs from checkedCities
+    const cityIds = checkedCities.map(function (city) {
+      return city.id;
+    });
+
+    // Create a data object to send in the request
+    const requestData = {
+      amenities: amenityIds,
+      states: stateIds,
+      cities: cityIds
+    };
+
+    // Check if amenities, states and cities arrays are empty
+    if (!requestData.amenities.length && !requestData.states.length && !requestData.cities.length) {
+      // If all are empty, send an empty object to retrieve all places
+      requestData.amenities = requestData.states = requestData.cities = {};
+    }
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      data: JSON.stringify(requestData),
+      success: function (data) {
+        $('.places').empty(); // Clear existing results
+        for (const place of data) {
+          const article = createArticle(place);
+          $('.places').append(article);
+        }
+        console.log('Post request done successfully');
+      }
+    });
+  }
+
+  postRequest();
+
+  // Event listener for the "Search" button
+  $('button[type="button"]').click(function () {
+    postRequest();
   });
-  
+});
